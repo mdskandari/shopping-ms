a63a8327cf5d23c64882a5f45c188996bb41cd76
Mohammad Eskandari <mdskandari@outlook.com>
2023-05-02 11:27:00 +0330

    rename

--- customer/src/api/app-event.js
+++ customer/src/api/app-event.js
@@ -3,8 +3,8 @@ const CustomerService = require('../services/customer-service');
 module.exports = (app) =>{
     const service = new CustomerService();
 
-    app.use('/app-event',async(req,res,next)=>{
-        const payload = req.body;
+    app.use('/app-events',async(req,res,next)=>{
+        const {payload} = req.body;
         service.SubscribeEvents(payload);
 
         console.log("===================== Customer service Recovered Event =====================");
